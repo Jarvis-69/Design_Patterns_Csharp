9af426ff4f0ca81d85b39a55182253376635cdc5
Jarvis <joris.salvador@icloud.com>
2024-12-30 12:30:48 +0100

    file update

--- AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
+++ AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2a3eb2e5156c06f2b2f42af536532249af1236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273e89df0591bafde6e464ae5a369d0c5afcad30")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
